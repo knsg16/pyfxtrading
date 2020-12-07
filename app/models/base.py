7e97ca5c30421d6b45ef9ead82e2c483c7b56c0a
Yuuki Kanasugi <knsg16@gmail.com>
2020-12-07 14:11:01 +0900

    created method for db access

--- app/models/base.py
+++ app/models/base.py
@@ -1,7 +1,7 @@
-from sqlalchemy import Column
-from sqlalchemy import DateTime
-from sqlalchemy import Float
-from sqlalchemy import Integer
+from contextlib import contextmanager
+import logging
+import threading
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -9,27 +9,30 @@ from sqlalchemy.orm import scoped_session
 
 import settings
 
+logger = logging.getLogger(__name__)
 Base = declarative_base()
 engine = create_engine(f'sqlite:///{settings.db_name}?check_same_thread=False')
 Session = scoped_session(sessionmaker(bind=engine))
-
-class BaseCandleMixin(object):
-    time = Column(DateTime, primary_key=True, nullable=False)
-    open = Column(Float)
-    close = Column(Float)
-    high = Column(Float)
-    low = Column(Float)
-    volume = Column(Integer)
+lock = threading.Lock()
 
 
-class UsJpyBaseCandle1H(BaseCandleMixin, Base):
-    __tablename__ = "USD_JPY_1H"
+@contextmanager
+def session_scope():
+    session = Session()
+    session.expire_on_commit = False
+    try:
+        lock.acquire()
+        yield session
+        session.commit()
+    except Exception as e:
+        logger.error(f'action=session_scope error={e}')
+        session.rollback()
+        raise
+    finally:
+        session.expire_on_commit = True
+        lock.release()
 
-class UsJpyBaseCandle1M(BaseCandleMixin, Base):
-    __tablename__ = "USD_JPY_1M"
-
-class UsJpyBaseCandle5S(BaseCandleMixin, Base):
-    __tablename__ = "USD_JPY_5S"
 
 def init_db():
+    import app.models.candle
     Base.metadata.create_all(bind=engine)
--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 import logging
 import sys
+import datetime
 
-from oanda.oanda import Order
-from oanda.oanda import APIClient
-
+from app.models.candle import UsJpyBaseCandle1M
 import settings
 
 logging.basicConfig(level=logging.INFO, stream=sys.stdout)
 
 if __name__ == "__main__":
     import app.models
+
+    now1 = datetime.datetime(2020, 1, 2, 3, 4, 5)
+    UsJpyBaseCandle1M.create(now1, 1.0, 2.0, 3.0, 4.0, 5)
+    candle = UsJpyBaseCandle1M.get(now1)
+    print(candle.time)
+    print(candle.open)
+    candle.open = 100.0
+    candle.save()
+
+    updated_candle = UsJpyBaseCandle1M.get(now1)
+    print(updated_candle.open)
 
 
