98e008f5eeef98a8df3d77e7c632b6d452c575d2
Yuuki Kanasugi <knsg16@gmail.com>
2020-12-07 13:44:05 +0900

    created db

--- main.py
+++ main.py
@@ -9,16 +9,6 @@ import settings
 logging.basicConfig(level=logging.INFO, stream=sys.stdout)
 
 if __name__ == "__main__":
-    api_client = APIClient(settings.access_token, settings.account_id)
-
-    order = Order(
-        product_code=settings.product_code,
-        side="BUY",
-        units=10,
-    )
-
-    trades = api_client.get_open_trade()
-    for t in trades:
-        api_client.trade_close(t.trade_id)
+    import app.models
 
 
