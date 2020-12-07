01e66a702667d69a97528f02d2f4864dc5eb5e26
Yuuki Kanasugi <knsg16@gmail.com>
2020-12-07 13:18:54 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import logging
+import sys
+
+from oanda.oanda import Order
+from oanda.oanda import APIClient
+
+import settings
+
+logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+
+if __name__ == "__main__":
+    api_client = APIClient(settings.access_token, settings.account_id)
+
+    order = Order(
+        product_code=settings.product_code,
+        side="BUY",
+        units=10,
+    )
+
+    trades = api_client.get_open_trade()
+    for t in trades:
+        api_client.trade_close(t.trade_id)
+
+
