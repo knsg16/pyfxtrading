2c304f3361778028cec7eb574d22e2d8cd175191
Yuuki Kanasugi <knsg16@gmail.com>
2020-12-14 14:59:08 +0900

    #28

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 from threading import Thread
 
 from app.controllers.streamdata import stream
+from app.controllers.webserver import start
 import app.models
 
 import settings
@@ -16,10 +17,9 @@ if __name__ == "__main__":
     # streamThread.start()
     # streamThread.join()
 
-    from app.models.dfcandle import DataFrameCandle
-    df = DataFrameCandle(settings.product_code, settings.trade_duration)
-    df.set_all_candles(settings.past_period)
-    print(df.value)
+    serverThread = Thread(target=start)
+    serverThread.start()
+    serverThread.join()
 
 
 
